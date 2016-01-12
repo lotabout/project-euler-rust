806069c7c00a8337ddc621777e5f3978cd399734
zhang_ji <zhang_ji@worksap.co.jp>
2016-01-12 14:20:56 +0800

    [prime] optimize with wheel factorization

--- src/prime.rs
+++ src/prime.rs
@@ -7,8 +7,88 @@
 use std::cmp::Ordering;
 use std::collections::BinaryHeap;
 
+// Wheel will be an iterator, given small primes
+//
+// # Example
+// let mut wheel = Wheel::new([2,3].to_vec());
+// println!('{}', wheel.next().wrap());
+struct Wheel {
+    initials: std::vec::IntoIter<u64>,
+
+    // for cycling increment
+    steps: Vec<u64>,
+    current_step: usize,
+
+    // current position
+    next: u64,
+
+    // running status
+    state: WheelState,
+}
+
+enum WheelState {
+    Front,
+    Back,
+}
+
+impl Wheel {
+    pub fn new(v: Vec<u64>) -> Self {
+        let product = v.iter().fold(1, |x, y| x * y);
+        let mut initials = v.to_vec();
+        let mut spokes = Vec::new();
+
+        for i in *v.last().unwrap()..(product+1) {
+            if v.iter().filter(|&x| i % x == 0).count() <= 0 {
+                // valide spoke
+                initials.push(i);
+                spokes.push(i);
+            }
+        }
+        spokes.push(product+1);
+
+        let mut wheel_steps = Vec::new();
+        let mut last = 1;
+        for i in spokes {
+            wheel_steps.push(i - last);
+            last = i;
+        }
+
+        Wheel {
+            initials: initials.into_iter(),
+            steps: wheel_steps,
+            current_step: 0,
+            next: product+1,
+            state: WheelState::Front,
+        }
+    }
+}
+
+impl Iterator for Wheel {
+    type Item = u64;
+
+    fn next(&mut self) -> Option<u64> {
+        match self.state {
+            WheelState::Front => match self.initials.next() {
+                elt @ Some(..) => elt,
+                None => {
+                    self.state = WheelState::Back;
+                    Some(self.next)
+                }
+            },
+            WheelState::Back => {
+                self.next += self.steps[self.current_step];
+                self.current_step = (self.current_step + 1)%self.steps.len();
+                Some(self.next)
+            },
+        }
+    }
+}
+
+
 // store the running state of generating prime
 #[derive(Copy, Clone, Eq, PartialEq, Debug)]
+
+// a state will contain (prime, factor) which indicate that (prime * factor) will be crossed out
 struct RunningState {
     value: u64,
     prime: u64,
@@ -29,17 +109,17 @@ impl PartialOrd for RunningState{
 // The prime number generator
 pub struct Prime {
     running: BinaryHeap<RunningState>,
-    position: u64,
+    wheel: Wheel,
 }
+
+const SMALL_PRIMES: &'static [u64] = &[2,3,5,7];
 
 impl Prime {
     pub fn new() -> Self {
-        let ret = Prime {
+        Prime {
             running: BinaryHeap::new(),
-            position: 1,
-        };
-
-        ret
+            wheel: Wheel::new(SMALL_PRIMES.to_vec()),
+        }
     }
 }
 
@@ -47,45 +127,39 @@ impl Iterator for Prime {
     type Item = u64;
 
     fn next(&mut self) -> Option<Self::Item> {
-        // try next position
-        self.position += 1;
+        let mut next_try = self.wheel.next().unwrap();
 
-        // check if current position is already crossed out
-        //println!("begin");
-        //for i in self.running.iter() {
-            //println!("{:?}", i);
-        //}
-        while let Some(&RunningState{value, ..}) = self.running.peek() {
-            // self.position is a valid prime
-            if self.position != value {
+        while let Some(&RunningState{prime, value}) = self.running.peek() {
+            // next_try is a prime
+            if next_try < value {
                 break;
+            } else if next_try == value {
+                // try next value
+                next_try = self.wheel.next().unwrap();
             }
 
             // pop the current crossed value and insert the next one
-            while self.position == self.running.peek().unwrap().value {
-                if let Some(RunningState{value, prime}) = self.running.pop() {
-                    self.running.push(RunningState{
-                        value: value + prime,
-                        prime: prime,
-                    });
-                }
+            let mut next_value = value + prime;
+            while next_try > next_value{
+                next_value += prime;
             }
 
-            // try next position
-            self.position += 1;
+            self.running.pop();
+            self.running.push(RunningState{
+                prime: prime,
+                value: next_value,
+            });
         }
 
-        self.running.push(RunningState{
-            value: self.position * self.position,
-            prime: self.position,
-        });
+        // skip the initial primes
+        if next_try > 7 {
+            self.running.push(RunningState{
+                value: next_try * next_try,
+                prime: next_try,
+            });
+        }
 
-        //println!("After");
-        //for i in self.running.iter() {
-            //println!("{:?}", i);
-        //}
-
-        Some(self.position)
+        Some(next_try)
     }
 }
 
@@ -113,21 +187,44 @@ pub fn factors(orig: u64) -> Vec<u64> {
 #[cfg(test)]
 mod test {
     use super::Prime;
+    use super::Wheel;
+
+    const WHEEL_TEST: &'static [u64] = &[2, 3, 5, 7, 11, 13, 17, 19, 23, 25, 29, 31, 35, 37, 41, 43, 47, 49, 53];
+
+    #[test]
+    fn test_wheel() {
+        let mut wheel = Wheel::new([2,3].to_vec());
+
+        for p in WHEEL_TEST {
+            let current = wheel.next().unwrap();
+            assert_eq!(current, *p);
+        }
+    }
+
+
+    const SMALL_PRIMES_TEST: &'static [u64] = &[2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53,
+                                                59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109,
+                                                113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179,
+                                                181, 191, 193, 197, 199, 211, 223, 227, 229, 233, 239, 241,
+                                                251, 257, 263, 269, 271, 277, 281, 283, 293, 307, 311, 313,
+                                                317, 331, 337, 347, 349, 353, 359, 367, 373, 379, 383, 389,
+                                                397, 401, 409, 419, 421, 431, 433, 439, 443, 449, 457, 461,
+                                                463, 467, 479, 487, 491, 499, 503, 509, 521, 523, 541, 547,
+                                                557, 563, 569, 571, 577, 587, 593, 599, 601, 607, 613, 617,
+                                                619, 631, 641, 643, 647, 653, 659, 661, 673, 677, 683, 691,
+                                                701, 709, 719, 727, 733, 739, 743, 751, 757, 761, 769, 773,
+                                                787, 797, 809, 811, 821, 823, 827, 829, 839, 853, 857, 859,
+                                                863, 877, 881, 883, 887, 907, 911, 919, 929, 937, 941, 947,
+                                                953, 967, 971, 977, 983, 991, 997];
 
     #[test]
     fn test_prime() {
         let mut primes = Prime::new();
 
-        assert_eq!(primes.next(), Some(2));
-        assert_eq!(primes.next(), Some(3));
-        assert_eq!(primes.next(), Some(5));
-        assert_eq!(primes.next(), Some(7));
-        assert_eq!(primes.next(), Some(11));
-        assert_eq!(primes.next(), Some(13));
-        assert_eq!(primes.next(), Some(17));
-        assert_eq!(primes.next(), Some(19));
-        assert_eq!(primes.next(), Some(23));
-        assert_eq!(primes.next(), Some(29));
+        for p in SMALL_PRIMES_TEST {
+            let current = primes.next().unwrap();
+            assert_eq!(current, *p);
+        }
     }
 
     #[test]
